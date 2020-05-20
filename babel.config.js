79361ef18efe69aa62b06186b890b93a525b4244
lingodan <lingodan@163.com>
2020-05-20 23:04:26 +0800

    自定义Webpack和Babel配置

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,12 @@
 module.exports = {
-  presets: ["@vue/cli-plugin-babel/preset"]
+  presets: ["@vue/cli-plugin-babel/preset"],
+  // 按需引用 ==》https://www.antdv.com/docs/vue/introduce-cn/
+  // 如果使用babel配置，需要手动按需引用
+  //  安装模块 ==>cnpm i --save-dev babel-plugin-import ==> 安装到--save-dev空间下
+  plugins: [
+    [
+      "import",
+      { libraryName: "ant-design-vue", libraryDirectory: "es", style: "true" }
+    ] // `style: true` 会加载 less 文件
+  ]
 };
--- src/main.js
+++ src/main.js
@@ -2,8 +2,18 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import Antd from "ant-design-vue";
+// 引入ant-design-vue样式
+/*
+ 新项目直接引入报错：https://github.com/ant-design/ant-motion/issues/44
+  1.降低less版本
+  2.开启JavaScript使用（需要参考CLI的相关配置 ==》https://cli.vuejs.org/zh/config/#vue-config-js）
+ */
+import "ant-design-vue/dist/antd.less";
 
 Vue.config.productionTip = false;
+// 将ant-design-vue所有组件全局注入到Vue组件中
+Vue.use(Antd);
 
 new Vue({
   router,
