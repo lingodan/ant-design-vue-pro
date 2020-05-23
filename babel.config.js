40b3a2498da902fcc2f08a9049c525a95facf437
lingodan <lingodan@163.com>
2020-05-23 10:11:37 +0800

    babel 配置libraryName

--- babel.config.js
+++ babel.config.js
@@ -3,10 +3,8 @@ module.exports = {
   // 按需引用 ==》https://www.antdv.com/docs/vue/introduce-cn/
   // 如果使用babel配置，需要手动按需引用
   //  安装模块 ==>cnpm i --save-dev babel-plugin-import ==> 安装到--save-dev空间下
+  // 注意此处的libraryName要与Vue注入的的组件类型一致
   plugins: [
-    [
-      "import",
-      { libraryName: "ant-design-vue", libraryDirectory: "es", style: "true" }
-    ] // `style: true` 会加载 less 文件
+    ["import", { libraryName: "Antd", libraryDirectory: "es", style: "true" }] // `style: true` 会加载 less 文件
   ]
 };
